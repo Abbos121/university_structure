f8443608e6bf478a48e34a79084a751f0f694c1d
Abbos Akramov <abbosakramov121@gmail.com>
2023-05-21 08:54:12 +0500

    Initial commit

---
+++ src/main/java/com/self/university_structure/UniversityStructureApplication.java
@@ -0,0 +1,13 @@
+package com.selt.university_structure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityStructureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversityStructureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/self/university_structure/UniversityStructureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selt.university_structure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityStructureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
